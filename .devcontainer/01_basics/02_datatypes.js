48545b3d5d6d968d03b1272e2b80defebd5dd4db
ashishyash <gupta.ashish@yash.com>
2024-06-08 16:53:27 +0000

    data type s

---
+++ .devcontainer/01_basics/02_datatypes.js
@@ -0,0 +1,14 @@
+"use strict" // refer newer version of javascript code 
+
+// data types 
+
+// Number 2 to the power 53
+// bigInt : very large number 
+// String : any string or character 
+// boolean : for boolean values true/false
+// undefined : if any varibale not assinged any value then it will have value undefiend
+// symbol : for unique 
+
+
+// Object
+{}
