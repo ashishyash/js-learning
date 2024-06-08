7e5fc10102577023f99023b887c8f065655b173a
ashishyash <gupta.ashish@yash.com>
2024-06-08 16:28:28 +0000

    let var const

---
+++ .devcontainer/01_basics/01_variable.js
@@ -0,0 +1,13 @@
+const accountId = 1234;
+let accountName = 'Ganesh';
+var accountPassword = 22222;
+accountCity = 'Ujjain';
+
+let accountType;
+
+// accountId = 8888; this is not allowed
+accountName = 'Mahesh';
+accountPassword = 99999;
+accountCity = 'Kashi'
+
+console.table([accountId, accountName, accountPassword, accountCity, accountType]);
